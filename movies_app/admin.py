22f8900350d55b5ff8cf5a2709c1ba67457f0db8
Szymon Płaza <plazik3290@gmail.com>
2021-07-13 10:20:38 +0200

    create actors,directors,movies models and register movies in admin

--- movies_app/admin.py
+++ movies_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Movie
 
-# Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'year_of_production',
+                    'image', 'description')
+
+
+admin.site.register(Movie, MovieAdmin)
--- movies_app/models.py
+++ movies_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Actor(models.Model):
+    name = models.CharField(max_length=30)
+    date_of_birth = models.DateField(default="2010-01-01")
+    photo = models.URLField(max_length=300)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=30)
+    year_of_production = models.DateField(default="2010-01-01")
+    image = models.URLField(max_length=300)
+    description = models.TextField()
+    actors = models.ForeignKey(Actor, on_delete=models.CASCADE, blank=True)
+
+
+class Director(models.Model):
+    name = models.CharField(max_length=30)
+    date_of_birth = models.DateField(default="2010-01-01")
+    photo = models.URLField(max_length=300)
+    movies = models.ForeignKey(Movie, on_delete=models.CASCADE)
