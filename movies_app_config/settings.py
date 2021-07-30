e3d243d6aa83041bbc50bf80f390f35c247d234d
Szymon Płaza <plazik3290@gmail.com>
2021-07-30 10:50:38 +0200

    update main settings

--- movies_app_config/settings.py
+++ movies_app_config/settings.py
@@ -98,10 +98,21 @@ WSGI_APPLICATION = 'movies_app_config.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_HOST"),
+        'PORT': config("DB_PORT"),
+    }
+}
+
+"""DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}"""
 
 
 # Password validation
